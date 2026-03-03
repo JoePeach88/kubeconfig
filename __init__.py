3fd89a40ad6f341a7aeba259b0fc6448a443eeab
Egor Gaev <gaev5941@gmail.com>
2026-03-03 14:58:07 +0300

    added fixes, improvements and new functions

--- __init__.py
+++ __init__.py
@@ -1,21 +1,25 @@
 import shutil
 import os
 import glob
+import yaml
 from pathlib import Path
-from libs.helpers import print_message, print_choices, ERROR
-from utils import set_env_variable
+from helpers import print_message, print_choices, ERROR
+from helpers.kubeconfig.utils import set_env_variable
 
 
 # Module global settings
 __module_disabled_methods__ = []
 __module_name__ = 'KubeConfigHelper'
 __module_author__ = 'JoePeach88'
-__module_version__ = '1.0.0'
-__module_link__ = 'https://github.com/JoePeach88/kube-helper-kubeconfig'
+__module_version__ = '1.1.0'
+__module_link__ = 'https://github.com/JoePeach88/kubeconfig'
 __methods_static_aliases__ = {
     'ls': ['ll', 'list'],
     'store': ['save'],
-    'rm': ['remove', 'delete', 'drop']
+    'rm': ['remove', 'delete', 'drop'],
+    'use': ['set'],
+    'validate': ['check', 'syntax'],
+    'show': ['print']
 }
 
 
@@ -26,7 +30,7 @@ class kubeconfigHelper:
     def __init__(self, settings: dict):
         self.settings = settings
 
-    def store(self, kubeconfig: str, dest: str = None):
+    def store(self, kubeconfig: str, dest: str = None, use: bool = False):
         """
         Method stores kubeconfig to specified location `dest` or to default kubeconfigs location from config file.
         Usage:
@@ -34,6 +38,8 @@ class kubeconfigHelper:
                 kubeconfig store --kubeconfig /path/to/kubeconfig
             2. With specified location
                 kubeconfig store --kubeconfig /path/to/kubeconfig --dest /path/to/save/kubeconfig
+            3. Store and use stored kubeconfig.
+                kubeconfig store --kubeconfig /path/to/kubeconfig --use
         """
         try:
             dest_path = Path(self.settings.get('kubeconfigs_location', ''))
@@ -43,6 +49,8 @@ class kubeconfigHelper:
             dest_path = Path(dest_path / Path(kubeconfig).name)
             shutil.copy2(kubeconfig, dest_path)
             print_message(f"kubeconfig '{kubeconfig}' copied to '{dest_path}' successfully.")
+            if use:
+                self.use(str(dest_path))
         except FileNotFoundError:
             print_message(f"The source file '{kubeconfig}' was not found.", ERROR)
         except PermissionError:
@@ -50,7 +58,7 @@ class kubeconfigHelper:
         except Exception as e:
             print_message(f"An unexpected error occurred: {e}", ERROR)
 
-    def ls(self, pretty: bool = True, location: str = 'default,env,kubeconfigs_location'):
+    def ls(self, pretty: bool = True, location: str = 'all'):
         """
         Method displays all available kubeconfigs from all known kubeconfigs locations.
         Usage:
@@ -58,7 +66,7 @@ class kubeconfigHelper:
         """
         kubeconfig_list = []
         locations = location.split(',')
-        if 'env' in locations:
+        if 'env' in locations or 'all' in locations:
             # Check KUBECONFIG environment variable
             kubeconfig_env = os.getenv('KUBECONFIG')
             if kubeconfig_env:
@@ -67,11 +75,11 @@ class kubeconfigHelper:
                 paths = kubeconfig_env.split(separator)
 
                 for path_str in paths:
-                    path = Path(path_str.strip()).expanduser().resolve()
+                    path = Path(path_str.strip().strip(separator)).expanduser().resolve()
                     if path.exists() and path.is_file() and str(path) not in kubeconfig_list:
                         kubeconfig_list.append(str(path))
-        
-        if 'default' in locations:
+
+        if 'default' in locations or 'all' in locations:
             # Check default location
             default_config = Path.home() / '.kube' / 'config'
             if default_config.exists() and default_config.is_file():
@@ -79,7 +87,7 @@ class kubeconfigHelper:
                 if str(default_config) not in kubeconfig_list:
                     kubeconfig_list.append(str(default_config))
 
-        if 'kubeconfigs_location' in locations:
+        if 'kubeconfigs_location' in locations or 'all' in locations:
             # Check settings location
             if self.settings:
                 kubeconfigs_location = self.settings.get('kubeconfigs_location')
@@ -95,6 +103,7 @@ class kubeconfigHelper:
     def use(self, kubeconfig: str = None):
         """
         Method sets kubeconfig which will be used in future in kubectl, it sets selected kubeconfig to env variable.
+        NOTE: This command restarts all parent process to refresh env variables (Windows only).
         Usage:
             1. With prompt to select available kubeconfigs.
                 kubeconfig use
@@ -109,24 +118,88 @@ class kubeconfigHelper:
             current_kubeconfig = os.environ.get('KUBECONFIG', '').split(separator)
             if kubeconfig not in current_kubeconfig:
                 current_kubeconfig.append(kubeconfig)
-            kubeconfig = current_kubeconfig
-            set_env_variable('KUBECONFIG', separator.join(kubeconfig))
-            print_message(f"KUBECONFIG variable successfully set to '{separator.join(kubeconfig)}'")
+            kubeconfigs = current_kubeconfig
+            for kubeconfig in kubeconfigs:
+                if not kubeconfig:
+                    kubeconfigs.remove(kubeconfig)
+            set_env_variable('KUBECONFIG', separator.join(kubeconfigs))
+            print_message(f"KUBECONFIG variable successfully set to '{separator.join(kubeconfigs)}'")
 
-    def rm(self, kubeconfig: str = None):
+    def rm(self, kubeconfig: str = None, location: str = 'env', force: str = False):
         """
         Method removes kubeconfig from env variable KUBECONFIG.
+        NOTE: This command restarts all parent process to refresh env variables (Windows only).
         Usage:
-            kubeconfig rm --kubeconfig /path/to/kubeconfig
+            1. With prompt to select available kubeconfigs.
+                kubeconfig rm
+            2. With specified kubeconfig.
+                kubeconfig rm --kubeconfig /path/to/kubeconfig
+            3. Remove kubeconfig file.
+                kubeconfig rm --force
         """
         if not kubeconfig:
-            kubeconfig_list = self.ls(pretty=False, location='env')
+            kubeconfig_list = self.ls(pretty=False, location=location)
             kubeconfig = print_choices(kubeconfig_list, exit_btn=True)
         if kubeconfig:
+            if force:
+                os.unlink(kubeconfig)
+                print_message(f"kubeconfig file '{kubeconfig}' deleted.")
             separator = ';' if os.name == 'nt' else ':'
             current_kubeconfig = os.environ.get('KUBECONFIG', '').split(separator)
             if kubeconfig in current_kubeconfig:
                 current_kubeconfig.remove(kubeconfig)
-            kubeconfig = current_kubeconfig
-            set_env_variable('KUBECONFIG', separator.join(kubeconfig))
-            print_message(f"KUBECONFIG variable successfully set to '{separator.join(kubeconfig)}'")
+                kubeconfigs = current_kubeconfig
+                for kubeconfig in kubeconfigs:
+                    if not kubeconfig:
+                        kubeconfigs.remove(kubeconfig)
+                set_env_variable('KUBECONFIG', separator.join(kubeconfigs))
+                print_message(f"KUBECONFIG variable successfully set to '{separator.join(kubeconfigs)}'")
+            else:
+                print_message(f"kubeconfig '{kubeconfig}' not found in KUBECONFIG variable.", force=True)
+
+    def validate(self, kubeconfig: str, return_value: bool = False):
+        """
+        Method validates kubeconfig.
+        Usage:
+            kubeconfig validate --kubeconfig /path/to/kubeconfig
+        """
+        try:
+            with open(kubeconfig, 'r', encoding='utf-8') as stream:
+                data = yaml.safe_load(stream)
+                if return_value:
+                    return yaml.dump(data)
+            return f"kubeconfig '{kubeconfig}' is valid."
+        except yaml.YAMLError:
+            return f"kubeconfig '{kubeconfig}' is invalid."
+
+    def show(self, *args, kubeconfig: str = None):
+        """
+        Method displays kubeconfig content.
+        Usage:
+            1. With prompt to select available kubeconfigs.
+                kubeconfig show
+            2. With specified kubeconfig.
+                kubeconfig show --kubeconfig /path/to/kubeconfig
+            3. With specified kubeconfig and data to return.
+                kubeconfig show --kubeconfig /path/to/kubeconfig clusters users
+        """
+        if not kubeconfig and not args:
+            kubeconfig_list = self.ls(pretty=False)
+            kubeconfig = print_choices(kubeconfig_list, exit_btn=True)
+        elif not kubeconfig and args:
+            kubeconfig = args[0]
+            args = list(args)
+            args.pop(0)
+            args = tuple(args)
+        if kubeconfig:
+            kubeconfig_data = self.validate(kubeconfig, return_value=True)
+            if not args:
+                return kubeconfig_data
+            else:
+                kubeconfig_data = yaml.safe_load(kubeconfig_data)
+                kubeconfig_return_data = {}
+                for arg in args:
+                    if kubeconfig_data.get(arg):
+                        kubeconfig_return_data.update({arg: kubeconfig_data.get(arg)})
+                if kubeconfig_return_data:
+                    return yaml.dump(kubeconfig_return_data)
--- utils.py
+++ utils.py
@@ -1,6 +1,53 @@
 import os
 import subprocess
 import platform
+import signal
+import time
+import psutil
+from helpers import print_message, ERROR
+
+
+current_parent = 0
+
+
+def restart_parent_tree(process: psutil.Process, deep: int, delay_seconds: float = 1.0):
+    global current_parent
+    parent = process.parent()
+    current_parent += 1
+    if current_parent == deep and deep != 0:
+        parent = None
+    if parent is None:
+        cmdline = process.cmdline()
+        parent_executable = cmdline
+        print_message(f"Parent executable: {parent_executable[0]}")
+
+        time.sleep(delay_seconds)
+
+        process.send_signal(signal.SIGTERM)
+        print_message(f"Sent SIGTERM to parent process {process.pid}")
+
+        subprocess.Popen(cmdline)
+        return True
+    return restart_parent_tree(parent, deep)
+
+
+def restart_parent_terminal_process(deep: int = 2):
+    parent_pid = os.getppid()
+    print_message(f"Parent terminal PID: {parent_pid}")
+
+    try:
+        parent_proc = restart_parent_tree(psutil.Process(parent_pid), deep)
+
+        return parent_proc
+
+    except psutil.NoSuchProcess:
+        print_message(f"Error: Parent process {parent_pid} not found.", ERROR)
+    except psutil.AccessDenied as e:
+        print_message(f"Error: Access denied when signaling parent process {parent_pid}: {e}", ERROR)
+    except Exception as e:
+        print_message(f"An error occurred while killing the parent: {e}", ERROR)
+
+    return False
 
 
 def set_env_variable(name, value):
@@ -10,18 +57,18 @@ def set_env_variable(name, value):
 
     if system == 'Windows':
         subprocess.call(
-            ['set', f'{name}={value}'],
+            ['setx', name, value],
             stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-            shell=True,
-            close_fds=False
+            stderr=subprocess.DEVNULL
         )
+        print_message(f"Variable successfully set.", force=True)
+        process_restarted = restart_parent_terminal_process(deep=0)
+        if process_restarted:
+            print_message('Parent process successfully restarted.', force=True)
+        
 
     elif system in ('Linux', 'Darwin'):
-        subprocess.call(
-            ['export', f'{name}="{value}"'],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-            shell=True,
-            close_fds=False
-        )
+        shell_profile = os.path.expanduser('~/.bashrc')
+        with open(shell_profile, 'a', encoding='utf-8') as f:
+            f.write(f'\nexport {name}="{value}"\n')
+        print_message(f"Added to '{shell_profile}'. Run 'source {shell_profile}' to apply.", force=True)
